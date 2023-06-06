f804a9af817ec307df76bf6af4bfe3d03a5342af
Sean C M <Sean.C.Mbogo@gmail.com>
2023-06-06 14:47:39 -0400

    Chapter 1 complete

--- partyinvites/main.go
+++ partyinvites/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"net/http"
 )
 
 type Rsvp struct {
 	Name, Email, Phone string
 	WillAttend         bool
+}
+
+type formData struct {
+	*Rsvp
+	Errors []string
 }
 
 var responses = make([]*Rsvp, 0, 10)
@@ -26,6 +32,66 @@ func loadTemplates() {
 	}
 }
 
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, nil)
+}
+
+func listHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["list"].Execute(writer, responses)
+}
+
+func formHandler(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		templates["form"].Execute(writer, formData{
+			Rsvp: &Rsvp{}, Errors: []string{},
+		})
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+		responseData := Rsvp{
+			Name:       request.Form["name"][0],
+			Email:      request.Form["email"][0],
+			Phone:      request.Form["phone"][0],
+			WillAttend: request.Form["willattend"][0] == "true",
+		}
+
+		errors := []string{}
+		if responseData.Name == "" {
+			errors = append(errors, "Please enter your name")
+		}
+		if responseData.Email == "" {
+			errors = append(errors, "Please enter your email address")
+		}
+		if responseData.Phone == "" {
+			errors = append(errors, "Please enter your phone number")
+		}
+		if len(errors) > 0 {
+			templates["form"].Execute(writer, formData{
+				Rsvp: &responseData, Errors: errors,
+			})
+		} else {
+
+			responses = append(responses, &responseData)
+			// The append function is used to append a value to a slice.
+			// The ampersand is used to create a pointer to the Rsvp value. Without it,
+			// the Rsvp value would be duplicated when added to the slice
+			if responseData.WillAttend {
+				templates["thanks"].Execute(writer, responseData.Name)
+			} else {
+				templates["sorry"].Execute(writer, responseData.Name)
+			}
+		}
+	}
+}
+
 func main() {
 	loadTemplates()
+
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/list", listHandler)
+	http.HandleFunc("/form", formHandler)
+
+	err := http.ListenAndServe(":4001", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
